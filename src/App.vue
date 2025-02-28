9296ce6dd2e3fd05185379abca10ee10ec116bc4
Roberto Torres <rtorres@softechpr.com>
2025-02-28 11:31:23 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <main>
+    <div class="main">
+      <Button label="Aplicar dark mode" @click="toggle" />
+    </div>
+  </main>
+</template>
+
+<script>
+import Button from 'primevue/button';
+
+export default {
+  data: () => ({
+    isDarkMode  : false,
+  }),
+  methods: {
+    toggle() {
+      if( this.isDarkMode )
+        document.documentElement.classList.remove('darkMode');
+      else
+        document.documentElement.classList.add('darkMode');
+
+      this.isDarkMode = !this.isDarkMode;
+    }
+  },
+  components: {
+    Button
+  }
+}
+</script>
+
+<style scoped>
+.main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+
+const app = createApp(App);
+
+app.use( PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            darkModeSelector: '.darkMode'
+        }
+    }
+});
+
+app.mount('#app');
